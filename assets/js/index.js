19bf03f7278ac7e2f9596f99ff469d1d8388993a
BuckEliza <124238923+BuckEliza@users.noreply.github.com>
2023-06-07 21:47:50 +0600

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger-btn").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open")
+    })
+})
